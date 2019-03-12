d65a7d44d7ed412e7d0bb05a021495475f13c201
ShiminLei <sl2kd@virginia.edu>
2019-03-11 23:28:27 -0400

    c

--- 707. Design Linked List.py
+++ 707. Design Linked List.py
@@ -1,5 +1,7 @@
-class Node(object):
 
+
+
+class Node(object):
     def __init__(self, val):
         self.val = val
         self.next = None
@@ -20,23 +22,21 @@ class MyLinkedList(object):
         :type index: int
         :rtype: int
         """
-        if index < 0 or index >= self.size:
+        if index < 0 or index >= self.size:  # 也就是如果 self.size==0, index只能return-1
             return -1
-
         if self.head is None:
             return -1
 
         curr = self.head
-        for i in range(index):
+        for i in range(index):  # 这里如果 index 是0，则不会执行 for之后的命令
             curr = curr.next
         return curr.val
 
     def addAtHead(self, val):
         """
-        Add a node of value val before the first element of the linked list.
-        After the insertion, the new node will be the first node of the linked list.
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
         :type val: int
-        :rtype: void
+        :rtype: None
         """
         node = Node(val)
         node.next = self.head
@@ -48,7 +48,7 @@ class MyLinkedList(object):
         """
         Append a node of value val to the last element of the linked list.
         :type val: int
-        :rtype: void
+        :rtype: None
         """
         curr = self.head
         if curr is None:
@@ -57,21 +57,19 @@ class MyLinkedList(object):
             while curr.next is not None:
                 curr = curr.next
             curr.next = Node(val)
-
         self.size += 1
 
     def addAtIndex(self, index, val):
         """
         Add a node of value val before the index-th node in the linked list.
-        If index equals to the length of linked list, the node will be appended to the end of linked list.
+        If index equals to the length of linked list, the node will be appended to the end of linked list. 如果index 正好等于list的长度，就加在最后。
         If index is greater than the length, the node will not be inserted.
         :type index: int
         :type val: int
-        :rtype: void
+        :rtype: None
         """
         if index < 0 or index > self.size:
-            return
-
+            return  # return 就是什么都不操作
         if index == 0:
             self.addAtHead(val)
         else:
@@ -81,18 +79,16 @@ class MyLinkedList(object):
             node = Node(val)
             node.next = curr.next
             curr.next = node
-
             self.size += 1
 
     def deleteAtIndex(self, index):
         """
         Delete the index-th node in the linked list, if the index is valid.
         :type index: int
-        :rtype: void
+        :rtype: None
         """
-        if index < 0 or index >= self.size:
+        if index < 0 or index >= self.size:  # 包含了 size 是0的情况
             return
-
         curr = self.head
         if index == 0:
             self.head = curr.next
@@ -102,4 +98,12 @@ class MyLinkedList(object):
             curr.next = curr.next.next
 
         self.size -= 1
+
+        # Your MyLinkedList object will be instantiated and called as such:
+        # obj = MyLinkedList()
+        # param_1 = obj.get(index)
+        # obj.addAtHead(val)
+        # obj.addAtTail(val)
+        # obj.addAtIndex(index,val)
+        # obj.deleteAtIndex(index)
 
--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 LeetCode collection.
 
-| Date | Question Numbers | 
-| :--- | :--: |
-| 3.11 | 160/ | 
+| Date | Question Numbers | Star! |
+| :--- | :--: | ---:|
+| 3.11 | 160/707/817 | 
 
 
