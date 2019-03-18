9082b95cb9f9e13faa87999b6b3eb91eb8a3cc33
ShiminLei <sl2kd@virginia.edu>
2019-03-18 18:20:16 -0400

    c

---
+++ 373. Find K Pairs with Smallest Sums.py
@@ -0,0 +1,15 @@
+# O(kLogk)
+def kSmallestPairs(nums1, nums2, k):
+    queue = []
+    def push(i, j):
+        if i < len(nums1) and j < len(nums2):
+            heapq.heappush(queue, [nums1[i] + nums2[j], i, j])
+    push(0, 0)
+    pairs = []
+    while queue and len(pairs) < k:
+        _, i, j = heapq.heappop(queue)
+        pairs.append([nums1[i], nums2[j]])
+        push(i, j + 1)
+        if j == 0:
+            push(i + 1, 0)
+    return pairs
